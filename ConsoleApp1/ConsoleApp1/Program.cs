b175bf5234abf75cd2cab3548bc4b29a79a8a057
tjforeman <tylerforeman92@gmail.com>
2020-03-04 21:34:34 -0500

    hello world tutorial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 15;
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine(age);
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello Tyler");
+            age = 20;
+            Console.WriteLine(age);
+            Console.Read();
+        }
+    }
+}
